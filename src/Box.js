eafa7f84609e6b821b8a5e9b27e0e0d3bd0beee1
Alison Feldish <alison.feldish@gmail.com>
2023-01-05 19:09:01 -0500

    build out list component

---
+++ src/Box.js
@@ -0,0 +1,19 @@
+import { getNodeText } from "@testing-library/react";
+import React, { Component } from "react";
+
+class Box extends Component {
+    render () {
+        return (
+            <div>
+                <div style={{
+                height: `${this.props.height}em`,
+                width: `${this.props.width}em`,
+                backgroundColor: this.props.color
+                }} />
+                <button>X</button>
+            </div>
+        );
+    };
+};
+
+export default Box;
